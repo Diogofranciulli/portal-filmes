ac671745dd2ffe9cb383495595a1518af2753af3
Diogo Leles Franciulli <54507748+Diogofranciulli@users.noreply.github.com>
2024-09-25 20:57:05 -0300

    pages filmes

--- src/pages/MovieListPage.jsx
+++ src/pages/MovieListPage.jsx
@@ -1,15 +1,65 @@
-import { useParams } from "react-router-dom"
-
+import { useState } from "react";
+import MovieCard from "../components/MovieCard";
+import movies from "../data/movies.json"
 export default function MovieListPage(){
-
-    const {id} = useParams()
+    //  const [contador, setContador] = useState(0)
+    //  const [textoBotao, settextoBotao] = useState('teste')
+    // const handleClick = () =>{
+    //     setContador((prev)=>(
+    //         prev + 1
+    //     ))
+    //     console.log(contador +1);
+    // }
+    // const diminuiClick = () =>{
+    //     setContador((prev)=>(
+    //         prev - 1
+    //     ))
+    //     console.log(contador - 1);
+    // }
+    // const zerarClick = () =>{
+    //     setContador((prev)=>(
+    //         prev * 0
+    //     ))
+    //     console.log(contador );
+    // }
+    
+    // const trocaPalavra = () =>{
+    //     settextoBotao((prev) => (prev === 'flaco' ? 'Lopes':'flaco'));
+    // }
+    const [search, setSearch] = useState("")
+    const handleSearch = (e) =>{
+        setSearch(e.target.value)
+        console.log(search);
+    }
+    const filmesFiltrados = movies.filter(filme => (
+        filme.titulo.toLowerCase().includes(search.toLowerCase())
+    ))
     return(
         <>
-        <h1 className="bg-black text-white">Movie List Page</h1>
-        <p>o id do filme é {id}</p>
-        {/* Exibe a lista de todos os filmes disponíveis categorizados por gênero.
-        Cada filme na lista é mostrado com uma imagem, 
-        título e um link para a página de detalhes do filme.*/}
+        <h1>Veja o catalago de filmes</h1>
+        <input
+         className="text-black"
+         type="text"
+         name="search"
+         id="search"
+         value={search}
+         onChange={handleSearch} />
+         <section className="flex">
+{ 
+            filmesFiltrados.map(filme => (
+                <MovieCard key ={filme.id} {...filme}/>
+            ))
+        }
+
+         </section>
+         
+        {/* <p>{contador}</p>
+        <button  onClick={ handleClick }>Aumentar</button><br></br>
+        <button onClick={ diminuiClick }>diminuir</button><br></br>
+        <button onClick={ zerarClick }>zerar</button><br></br>  
+
+        <p>{textoBotao}</p>
+        <button onClick={ trocaPalavra }>troque a palavra</button> */}
         </>
     )
 }
